2277e34f98b62b583957952ced3792f1615dea9a
Paul James <jpaul3703@gmail.com>
2022-09-08 12:42:55 -0700

    Task 4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+* main - print the string in the puts function
+*
+* Description: Using the puts function
+* This program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
