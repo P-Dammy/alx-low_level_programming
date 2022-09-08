20e9318d90bb42768cd1b2f586d98d85e4b8bd74
Paul James <jpaul3703@gmail.com>
2022-09-08 12:34:10 -0700

    Task 4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+int main(void)
 {
-     char Mystr[] = "Programming is like building a multilingual puzzle";
-     puts(Mystr);
-     return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
