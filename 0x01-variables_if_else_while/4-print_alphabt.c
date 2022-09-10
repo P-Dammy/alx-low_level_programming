0962a29d70bcc23ab19b14b4f90da260a4f0912c
Paul James <jpaul3703@gmail.com>
2022-09-09 21:55:27 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - program will assign a random number to the variable
+* n each time it is executed.
+* DEscription: This program will assign a random number to the
+* variable n each time is executed.
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
