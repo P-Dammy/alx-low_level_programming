34832c860d413d1868f46e2ff050112cb3749f17
Paul James <jpaul3703@gmail.com>
2022-09-09 22:06:11 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - program will assign a random number to the variable
+* n each time it is executed.
+* DEscription: This program will assign a random number to the
+* variable n each time is executed.
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'z' ; c <= 'a' ; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
