9bc27c205d9b399abb7f0e254cbfe1158eecf7fa
Paul James <jpaul3703@gmail.com>
2022-09-09 22:01:04 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - program will assign a random number to the variable
+* n each time it is executed.
+* DEscription: This program will assign a random number to the
+* variable n each time is executed.
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
