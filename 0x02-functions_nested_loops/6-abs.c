b43160192b01470ebc2a7bba086511ed7a28b673
Paul James <jpaul3703@gmail.com>
2022-10-21 05:30:34 -0700

    Task 6 correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,8 @@
 /**
  * _abs - Returns the absolute value of a number.
  * @n: THe number to be printed
+ * @r: the absolute value of n if negative
+ * Return: Returns the absolute value of n
  */
 
 int _abs(int n)
@@ -10,6 +12,7 @@ int _abs(int n)
 	if (n < 0)
 	{
 		int r;
+
 		r = n * -1;
 		return (r);
 	}
