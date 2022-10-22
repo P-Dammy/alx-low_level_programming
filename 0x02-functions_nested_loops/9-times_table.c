9257ff90e83eca0bba30c5a40be663e45410e9f8
Paul James <jpaul3703@gmail.com>
2022-10-22 09:11:49 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - Prints times table.
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int r = j * i;
+
+			_putchar(r + '0');
+			if (j < 9)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
