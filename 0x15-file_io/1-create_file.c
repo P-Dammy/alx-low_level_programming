1b6204d993a941f8f0d6821b9bfbe851264575aa
Paul James <jpaul3703@gmail.com>
2022-11-05 04:57:57 -0700

    Task 1 correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,11 @@ int create_file(const char *filename, char *text_content);
 
 	if (text_content != NULL)
 	{
-		for (len = 0, text_content[len];)
-			len++;
+		for (len = 0, text_content[len]; len++)
+			;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
