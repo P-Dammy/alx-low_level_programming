99739a902ae18aa41a623d0bf5803dc40c83ecbd
Paul James <jpaul3703@gmail.com>
2022-11-05 04:48:20 -0700

    Task 1 correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * create_file - Creates a file.
@@ -22,7 +21,7 @@ int create_file(const char *filename, char *text_content);
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0500);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
