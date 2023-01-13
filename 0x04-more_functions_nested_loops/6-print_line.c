2b38c6634b07384b6a7aea5bdc6be25c9bebfeb6
Paul James <jpaul3703@gmail.com>
2023-01-13 13:07:49 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Prints _ n times to form a line
+ * @n : number of times to print _
+ *
+ * Returns - Always 0
+ */
+
+void print_line(int n)
+
+{
+	int a;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 1; a = n; a++)
+			_putchar('_');
+
+	_putchar('\n');
+	}
+}
