54eaa8b867d9cdde61e383599f493974257968d7
Paul James <jpaul3703@gmail.com>
2023-01-15 10:47:31 -0800

    Advanced task 2 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,7 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpa
-r, 2015-10-19\n", 59);
-
+	write(2, "and that piece of art is useful\" - Dora Ko
+rpar, 2015-10-19\n", 59);
 	return (1);
 }
