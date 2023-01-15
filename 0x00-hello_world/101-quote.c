2d04205a1f301272398e2ca3849a58d88084f690
Paul James <jpaul3703@gmail.com>
2023-01-15 10:24:05 -0800

    Advanced task 2 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,16 @@
 #include <unistd.h>
 
 /**
- * main - prints a statement
- * Return: Always 0
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line to the stabdard error
+ * Return: Always 1
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora Korpar,
-2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Ko
+rpar, 2015-10-19\n", 59);
 
-return (1);
+	return (1);
 }
