f32d66dbd0980fd7d08141dcd0fb2e700c87d5c7
Paul James <jpaul3703@gmail.com>
2023-01-26 21:23:23 -0800

    Task 0 correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,16 @@
 
 char *_strcat(char *dest, const char *src)
 {
-int index = 0, dest_len = 0;
+int i = 0, j = 0;
 
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-
+while (dest[i] != '\0')
+i++;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
 return (dest);
 }
