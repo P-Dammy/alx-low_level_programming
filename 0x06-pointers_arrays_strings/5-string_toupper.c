d7d4c082aad81263a312c1a2f2b295dae90fefe5
P-Dammy <111010822+P-Dammy@users.noreply.github.com>
2023-05-10 15:18:18 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,13 @@
 
 char *string_toupper(char *n);
 {
-int i = 0;
+  int i = 0;
 
-while (n[i] != '\0')
-{
-if (n[i] >= 'a' && n[i] <= 'z')
-n[i] = n[i] - 32;
-i++;
-}
-return (n);
+  while (n[i] != '\0')
+  {
+    if (n[i] >= 'a' && n[i] <= 'z')
+      n[i] = n[i] - 32;
+    i++;
+  }
+  return (n);
 }
